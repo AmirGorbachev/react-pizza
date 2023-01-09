c0435b942a3f1ae752e1b5db3100d6a14802c91f
Amir Gorbachev <amir.gorbachyow@yandex.ru>
2023-01-09 12:41:44 +0700

    Using useRef

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -6,6 +6,12 @@ import style from "./Search.module.scss";
 
 const Search = () => {
   const { searchBy, setSearchBy } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchBy("");
+    inputRef.current.focus();
+  };
 
   return (
     <label className={style.root}>
@@ -13,6 +19,7 @@ const Search = () => {
         <path d='M344.5,298c15-23.6,23.8-51.6,23.8-81.7c0-84.1-68.1-152.3-152.1-152.3C132.1,64,64,132.2,64,216.3  c0,84.1,68.1,152.3,152.1,152.3c30.5,0,58.9-9,82.7-24.4l6.9-4.8L414.3,448l33.7-34.3L339.5,305.1L344.5,298z M301.4,131.2  c22.7,22.7,35.2,52.9,35.2,85c0,32.1-12.5,62.3-35.2,85c-22.7,22.7-52.9,35.2-85,35.2c-32.1,0-62.3-12.5-85-35.2  c-22.7-22.7-35.2-52.9-35.2-85c0-32.1,12.5-62.3,35.2-85c22.7-22.7,52.9-35.2,85-35.2C248.5,96,278.7,108.5,301.4,131.2z' />
       </svg>
       <input
+        ref={inputRef}
         className={style.input}
         type='text'
         placeholder='Поиск пиццы...'
@@ -22,7 +29,7 @@ const Search = () => {
       {searchBy && (
         <svg
           className={style.iconClear}
-          onClick={() => setSearchBy("")}
+          onClick={() => onClickClear()}
           height='48'
           viewBox='0 0 48 48'
           width='48'
