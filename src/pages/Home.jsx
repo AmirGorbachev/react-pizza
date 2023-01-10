fb5993ddfe4e32001144fd88f4d348ccdabf2880
Amir Gorbachev <amir.gorbachyow@yandex.ru>
2023-01-10 15:03:43 +0700

    Add saving filter data in url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import axios from "axios";
+import qs from "qs";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { setTotalPages } from "../store/slices/filterSlice";
+import { setTotalPages, setFilters } from "../store/slices/filterSlice";
 
 import Sort from "../components/Sort";
 import Categories from "../components/Categories";
@@ -13,35 +15,79 @@ import Pagination from "../components/Pagination";
 
 function Home() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [pizzas, setPizzas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  let hasUrlParams = React.useRef(false);
+  let isMounted = React.useRef(false);
 
   // components
   const { category, sort, isOrderAsc, currentPage, searchBy } = useSelector(
     (state) => state.filter
   );
 
+  // При первом рендере сохранять параметры в сторе
   React.useEffect(() => {
-    setIsLoading(true);
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
 
-    const sortMask = `&sortBy=${sort.value}`;
-    const orderMask = isOrderAsc ? "&order=acs" : "&order=desc";
-    const categoryMask = category > 0 ? `&category=${category}` : "";
-    const searchMask = searchBy !== "" ? `&search=${searchBy}` : "";
+      dispatch(
+        setFilters({
+          category: params.category,
+          sort: params.sort,
+          currentPage: params.currentPage,
+          searchBy: params.searchBy,
+          isOrderAsc: params.isOrderAsc,
+        })
+      );
 
-    axios
-      .get(
-        `https://63b84b4e6f4d5660c6d29fea.mockapi.io/pizzas?page=${currentPage}&limit=4${sortMask}${orderMask}${categoryMask}${searchMask}`
-      )
-      .then((data) => {
-        setPizzas(data.data.items);
-        dispatch(setTotalPages(Math.ceil(data.data.count / 4)));
-        setIsLoading(false);
+      hasUrlParams.current = true;
+    }
+  }, [dispatch]);
+
+  // При последующих рендерах записывать параметры в URL
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        category,
+        sort: sort,
+        searchBy,
+        currentPage,
+        isOrderAsc,
       });
 
+      navigate(`?${queryString}`);
+    }
+
+    isMounted.current = true;
+  }, [category, sort, isOrderAsc, searchBy, currentPage, navigate]);
+
+  // Загрузка данных
+  React.useEffect(() => {
     window.scrollTo(0, 0);
-  }, [category, sort, isOrderAsc, searchBy, currentPage]);
+
+    if (!hasUrlParams.current) {
+      setIsLoading(true);
+
+      const sortMask = `&sortBy=${sort}`;
+      const orderMask = isOrderAsc ? "&order=acs" : "&order=desc";
+      const categoryMask = category > 0 ? `&category=${category}` : "";
+      const searchMask = searchBy !== "" ? `&search=${searchBy}` : "";
+
+      axios
+        .get(
+          `https://63b84b4e6f4d5660c6d29fea.mockapi.io/pizzas?page=${currentPage}&limit=4${sortMask}${orderMask}${categoryMask}${searchMask}`
+        )
+        .then((data) => {
+          setPizzas(data.data.items);
+          dispatch(setTotalPages(Math.ceil(data.data.count / 4)));
+          setIsLoading(false);
+        });
+    }
+
+    hasUrlParams.current = false;
+  }, [category, sort, isOrderAsc, searchBy, currentPage, dispatch]);
 
   return (
     <>
